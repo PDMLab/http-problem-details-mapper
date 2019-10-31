618ec69d54c0709078426250ec3e07ba115467db
tpluscode <tpluscode@users.noreply.github.com>
2019-10-31 11:05:02 +0100

    fix: registry should return a matching mapper for derived class

--- src/DefaultErrorMapper.ts
+++ src/DefaultErrorMapper.ts
@@ -6,9 +6,7 @@ export class DefaultErrorMapper extends ErrorMapper {
   public constructor () {
     super(Error)
   }
-  public mapError (error: Error): ProblemDocument {
+  public mapError (): ProblemDocument {
     return StatusCodeErrorMapper.mapStatusCode(500)
   }
-
-  public error: string;
 }
--- test/MapperRegistryTest.ts
+++ test/MapperRegistryTest.ts
@@ -1,9 +1,10 @@
 import 'should'
-import { ErrorMapper, MapperRegistry } from '../src'
+import { DefaultErrorMapper, ErrorMapper, MapperRegistry } from '../src'
 import { ProblemDocument } from 'http-problem-details'
 
 class FooError extends Error {}
 class BarError extends Error {}
+class ChildFooError extends FooError {}
 
 class FooErrorMapper extends ErrorMapper {
   public constructor () {
@@ -33,5 +34,23 @@ describe('MapperRegistry', (): void => {
     const mapper = registry.getMapper(new FooError())
 
     mapper.should.be.instanceOf(FooErrorMapper)
+  })
+
+  it('returns a mapper for a matched superclass', (): void => {
+    const registry = new MapperRegistry()
+      .registerMapper(new FooErrorMapper())
+
+    const mapper = registry.getMapper(new ChildFooError())
+
+    mapper.should.be.instanceOf(FooErrorMapper)
+  })
+
+  it('returns default mapper unmapped class', (): void => {
+    const registry = new MapperRegistry()
+      .registerMapper(new BarErrorMapper())
+
+    const mapper = registry.getMapper(new ChildFooError())
+
+    mapper.should.be.instanceOf(DefaultErrorMapper)
   })
 })
