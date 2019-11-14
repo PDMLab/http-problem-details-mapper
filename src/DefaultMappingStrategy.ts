cee321c53d83e7bf45533176dfc50643c588603d
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2019-11-14 21:28:29 +0100

    feat: introduce default mapping strategy

---
+++ src/DefaultMappingStrategy.ts
@@ -0,0 +1,21 @@
+import { ProblemDocument } from 'http-problem-details'
+import { IMappingStrategy } from './IMappingStrategy'
+import { MapperRegistry } from './MapperRegistry'
+
+export class DefaultMappingStrategy implements IMappingStrategy {
+  public registry: MapperRegistry
+
+  public constructor (registry: MapperRegistry) {
+    this.registry = registry
+  }
+
+  public map (error: any): ProblemDocument | null {
+    const err = error
+    const errorMapper = this.registry.getMapper(error)
+    if (errorMapper) {
+      return errorMapper.mapError(err)
+    }
+
+    return null
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ import { DefaultErrorMapper } from './DefaultErrorMapper'
 import { StatusCodeErrorMapper } from './StatusCodeErrorMapper'
 import { IMappingStrategy, MappingStrategy } from './IMappingStrategy'
 import { ErrorStatusCodes } from './ErrorStatusCodes'
+import { DefaultMappingStrategy } from './DefaultMappingStrategy'
 
 export {
+  DefaultMappingStrategy,
   ErrorStatusCodes,
   ErrorMapper,
   IErrorMapper,
